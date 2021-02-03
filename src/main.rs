664d322b9be7516df9fc28735f0623c384a6401d
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-02-03 11:01:30 -0500

    feat: downloads specific folder into new project app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use async_recursion::async_recursion;
 use exitfailure::ExitFailure;
 use reqwest::header::USER_AGENT;
 use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
-use std::fs;
+use std::fs::{create_dir_all, File};
+use std::io;
 use structopt::StructOpt;
 use termion::color;
 
@@ -19,7 +21,7 @@ struct Cli {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct GHResponse {}
+struct BpHandler {}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Details {
@@ -34,7 +36,7 @@ struct Details {
     r#type: String,
 }
 
-impl GHResponse {
+impl BpHandler {
     async fn get_types(&self) -> Result<Vec<String>, ExitFailure> {
         let content = self.get_content("/").await?;
 
@@ -49,7 +51,7 @@ impl GHResponse {
     }
     async fn get_content(&self, endpoint: &str) -> Result<Vec<Details>, ExitFailure> {
         let url_str = format!(
-            "https://api.github.com/repos/jhonatanmacazana/vscode-boilerplates/contents{}",
+            "https://api.github.com/repos/jhonatanmacazana/vscode-boilerplates/contents/{}",
             endpoint
         );
         let url = Url::parse(&*url_str)?;
@@ -64,13 +66,61 @@ impl GHResponse {
         let parsed: Vec<Details> = serde_json::from_str(&txt)?;
         Ok(parsed)
     }
+
+    #[async_recursion]
+    async fn create_dir_from_template(
+        &self,
+        web_content_path: &str,
+        parent_folder_path: &str,
+    ) -> Result<(), ExitFailure> {
+        create_dir_all(parent_folder_path)?;
+        let path_content = self.get_content(&web_content_path).await?;
+        for elem in path_content {
+            let file_path = format!("{}/{}", parent_folder_path, elem.name);
+            if elem.r#type == "file" {
+                self.copy_web_content_to_file(&elem.download_url.unwrap(), &file_path)
+                    .await?;
+            } else if elem.r#type == "dir" {
+                let child_dir_path = format!("{}/{}", parent_folder_path, elem.name);
+                self.create_dir_from_template(&elem.path, &child_dir_path)
+                    .await?;
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn copy_web_content_to_file(
+        &self,
+        web_url: &str,
+        path_file: &str,
+    ) -> Result<(), ExitFailure> {
+        let mut out = File::create(path_file)?;
+
+        let client = reqwest::Client::new();
+        let resp = client
+            .get(web_url)
+            .header(USER_AGENT, "vsr-new rust app")
+            .send()
+            .await?;
+
+        let bytes = resp.bytes().await?;
+        let mut slice: &[u8] = bytes.as_ref();
+
+        io::copy(&mut slice, &mut out)?;
+        print!("{}  created ", color::Fg(color::Cyan));
+        println!("{}: {}", color::Fg(color::Reset), path_file);
+
+        Ok(())
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
+
     if args.types {
-        let gh_response = GHResponse {};
+        let gh_response = BpHandler {};
         println!("{}Fetching available types...", color::Fg(color::LightRed));
         let _resp = gh_response.get_types().await?;
         for elem in _resp {
@@ -78,12 +128,20 @@ async fn main() -> Result<(), ExitFailure> {
         }
         return Ok(());
     }
+
+    let gh_response = BpHandler {};
+
     println!(
-        "City: {:?}. CC: {:?}. F: {}",
-        args.template_type, args.new_app, args.types
+        "{}Fetching available types...",
+        color::Fg(color::LightGreen)
     );
 
-    fs::create_dir_all(args.new_app.unwrap())?;
+    let template_type = args.template_type.unwrap();
+    let new_app = args.new_app.unwrap();
+    println!("{}Creating {}", color::Fg(color::LightGreen), new_app);
+    gh_response
+        .create_dir_from_template(&template_type, &new_app)
+        .await?;
 
     Ok(())
 }
